69a022ef06a713643f6e06ad9802096166324344
Joao Gabriel da Silva Andrade <joaogabrielandrade7767@gmail.com>
2026-06-12 19:39:57 -0300

    Remove unnecessary blank lines in ListaCompras.java

--- ListaCompras.java
+++ ListaCompras.java
@@ -1,5 +1,3 @@
-
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,24 +10,20 @@ public class ListaCompras {
         this.nome = nome;
         this.itens = new ArrayList<>();
     }
-
     public void adicionarItem(ItemLista item) {
         itens.add(item);
     }
-
     public String getNome() {
         return nome;
     }
-
     public List<ItemLista> getItens() {
         return itens;
     }
-
     public Compra getCompra() {
         return compra;
     }
-
     public void setCompra(Compra compra) {
         this.compra = compra;
+        
     }
 }
